1050c23c31c97ee991ebd904b10570817f375002
PierreML-06 <pierre.mouilleseaux-lhuillier@edu.univ-fcomte.fr>
2024-12-03 15:08:42 +0100

    ccs + on continue le js

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,10 @@
 // *** LIGHTBOX ***
-
+//début 
 const lightBox = document.querySelector("#lightbox");
 const img = document.querySelectorAll("imgfull");
-
+// varible pendant 
+const lightBox2 = img.dataset.lightBox2;
+const img2 = lightBox.querySelector("img");
 
 
 lightBox.showModal();
@@ -10,6 +12,7 @@ lightBox.showModal();
 
 img.forEach((img) => {
     img.addEventListener("click", () => {
-       
+
+
     });
 
