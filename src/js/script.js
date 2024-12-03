b47e066146bb1b1b701c3647e63d7e986512f142
PierreML-06 <pierre.mouilleseaux-lhuillier@edu.univ-fcomte.fr>
2024-12-03 15:27:23 +0100

    comit fin

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,20 @@
 // *** LIGHTBOX ***
 //début 
-const lightBox = document.querySelector("#lightbox");
-const img = document.querySelectorAll("imgfull");
-// varible pendant 
-const lightBox2 = img.dataset.lightBox2;
-const img2 = lightBox.querySelector("img");
+const lightBox = document.querySelector("lightbox");
+const img = document.querySelectorAll("img");
 
-
-lightBox.showModal();
 
 
 img.forEach((img) => {
     img.addEventListener("click", () => {
 
-
+        const light = img.dataset.lightBox2;
+        const img2 = lightBox.querySelector("img2");
+        img2.src = light; 
+        lightBox.showModal(); 
+         });
     });
 
+    lightBox.addEventListener("click", () => {
+        lightBox.close();
+    });
